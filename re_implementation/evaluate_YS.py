29a2eab3040eae57954f887f803f8577238178fa
superegoo <sunyuxin3@163.com>
2022-12-10 00:35:03 +0100

    update indenpent evaluation

--- re_implementation/evaluate_YS.py
+++ re_implementation/evaluate_YS.py
@@ -2,6 +2,8 @@
 import numpy as np
 import tensorflow as tf
 import tensorflow_datasets as tfds
+from re_implementation import dataset_utils_EC
+from re_implementation.helpers import model_helper
 
 
 """The evaluate(
@@ -35,13 +37,14 @@ def evaluating(cvae, x_val):
     loss2 = cvae.continuous_bernoulli_loss(out_sample, output2['reconstruction'])  # label 0
     vae_loss2 = loss1+loss2
     elbo = tf.reduce_logsumexp(vae_loss2 - vae_loss, axis=0)
+    elbo = elbo - tf.math.log(tf.cast(100, dtype=tf.float32))
 
     return elbo
 
-    #thershold for loss etc?
+#thershold for loss etc?
 def thershold(model, x_latent, x_test):
-    x_mnist = model.predict(x_latent, batch_size=64) #array of data latent vectors
-    test_digit = model.predict(x_test, batch_size=1)  #your testdigit latent vector
+    x_mnist = model.predict(x_latent, batch_size=64)  #array of data  vectors
+    test_digit = model.predict(x_test, batch_size=1)  #your testdigit  vector
     #calc the distance
     from scipy.spatial import distance
     threshold = 0.3  #min eucledian distance
@@ -50,5 +53,34 @@ def thershold(model, x_latent, x_test):
         if dst <= threshold:
             return True
 
-# call this in train.py
-# pro_result = evaluating(cvae, x_val)
+
+checkpoint_epoch = '0004'
+dataset_type = 'grayscale'  #'grayscale'  'natural'
+dataset = 'cifar10'  #'mnist'  'fmnist' 'cifar10'  'svhn' 'gtsrb'
+decoder_dist = 'cBern'  #'cBern'  'cat'
+method = 'BC-LL'
+
+latent_dimensions = 20
+num_samples = 1
+
+if dataset_type == 'grayscale':
+    num_filter = 32
+    num_channel = 1
+
+elif dataset_type == 'natural':
+    num_filter = 64
+    num_channel = 3
+else:
+    raise ValueError('Undefined dataset type.')
+
+cvae = model_helper.CVAE(
+    num_channel=num_channel,
+    num_filter=num_filter,
+    latent_dimensions=latent_dimensions,
+    num_samples=num_samples,
+    decoder_dist=decoder_dist
+)
+
+cvae.load_weights(f'saved_models/{decoder_dist}/{dataset}/cvae-{method}/weights-{checkpoint_epoch}')
+x_train, x_val, _ = dataset_utils_EC.get_dataset(dataset, decoder_dist, dataset_type)
+pro_result = evaluating(cvae, x_val)
