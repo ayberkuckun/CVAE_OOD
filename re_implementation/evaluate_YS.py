d745f7a06ff0e25c2650ca3c99740590d7e46ab7
superegoo <sunyuxin3@163.com>
2022-12-09 19:52:17 +0100

    update evaluation2

--- re_implementation/evaluate_YS.py
+++ re_implementation/evaluate_YS.py
@@ -1,58 +1,7 @@
 
-
 import numpy as np
-from scipy.optimize import minimize
-from numpy.random import rand
 import tensorflow as tf
 import tensorflow_datasets as tfds
-import itertools
-from absl import flags
-
-
-####### reference
-# in_sample = next(iter(x_val))
-# in_sample = np.expand_dims(in_sample, axis=0)
-# cvae.num_samples = 100
-# output = cvae.predict(in_sample)
-# output2 = cvae.predict(in_sample)
-# cvae.appy_correction = True
-# loss = cvae.continuous_bernoulli_loss(in_sample, output2['reconstruction'])
-# loss2 = cvae.kl_divergence_loss(in_sample, output2['kl_divergence'])
-# loss = loss + loss2 # label 1
-# loss2 = cvae.continuous_bernoulli_loss(out_sample, output2['reconstruction'])  # label 0
-############# thershold for loss etc?
-
-
-_NORMALIZE = flags.DEFINE_string(
-    'normalize', default=None, help='Normalization to apply')
-
-dataset = 'mnist'
-batch_size = 64
-
-model_func = {
-    'metrics': [
-        tf.keras.metrics.Accuracy(),
-    ]
-}
-
-def getValData(dataset):
-    if dataset == "mnist":
-        (ds_train, ds_val, ds_test), ds_info = tfds.load(
-            "mnist", split=['train[:90%]', 'train[90%:]', 'test'], with_info=True)
-
-        ds_val = ds_val.map(
-            partial(preprocess, inverted=False, mode='grayscale',
-                    normalize=_NORMALIZE.value, dequantize=False,
-                    visible_dist='cont_bernoulli'),
-            num_parallel_calls=tf.data.experimental.AUTOTUNE)
-        ds_val = ds_val.cache()
-        ds_val = ds_val.batch(batch_size)
-        ds_val = ds_val.prefetch(tf.data.experimental.AUTOTUNE)
-
-        return ds_val
-
-    else:
-        return None
 
 
 """The evaluate(
@@ -65,23 +14,41 @@ def getValData(dataset):
     -callbacks=None, 
     -return_dict=False, return the list
 """
-def evaluating(model, dataset):
-    if dataset == "mnist":
-        ds_val = getValData(dataset)
-        # model.evaluate(ds_val)
-        model.evaluate_generator(generator=validation_generator,
-                                 workers=10,
-                                 use_multiprocessing=True,
-                                 verbose=0)
-        return model
-    else:
-        return None
 
+def evaluating(cvae, x_val):
 
+    # Evaluate on Validation data
+    cvae.evaluate(x_val)
 
-model.compile(
-    optimizer=tf.keras.optimizers.Adam(learning_rate=5e-4),
-    loss={'reconstruction': cvae.get_reconstruction_loss_func(),
-          'kl_divergence': cvae.kl_divergence_loss},
-    metrics=model_func["metrics"]
-)
+    # Computes an importance weighted log likelihood estimate.
+    in_sample = next(iter(x_val))
+    in_sample = np.expand_dims(in_sample, axis=0)
+    cvae.num_samples = 100
+    output = cvae.predict(in_sample)
+    cvae.appy_correction = True
+    loss1 = cvae.continuous_bernoulli_loss(in_sample, output['reconstruction'])
+    loss2 = cvae.kl_divergence_loss(in_sample, output['kl_divergence'])
+    vae_loss = loss1 + loss2    # label 1
+
+    out_sample = in_sample
+    output2 = cvae.predict(out_sample)
+    loss2 = cvae.continuous_bernoulli_loss(out_sample, output2['reconstruction'])  # label 0
+    vae_loss2 = loss1+loss2
+    elbo = tf.reduce_logsumexp(vae_loss2 - vae_loss, axis=0)
+
+    return elbo
+
+    #thershold for loss etc?
+def thershold(model, x_latent, x_test):
+    x_mnist = model.predict(x_latent, batch_size=64) #array of data latent vectors
+    test_digit = model.predict(x_test, batch_size=1)  #your testdigit latent vector
+    #calc the distance
+    from scipy.spatial import distance
+    threshold = 0.3  #min eucledian distance
+    for vector in x_mnist:
+        dst = distance.euclidean(vector, test_digit[0])
+        if dst <= threshold:
+            return True
+
+# call this in train.py
+# pro_result = evaluating(cvae, x_val)
