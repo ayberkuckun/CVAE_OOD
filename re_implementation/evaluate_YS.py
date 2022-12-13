f77c9849feb47ad34764c2ba369bcf942c41d319
superegoo <sunyuxin3@163.com>
2022-12-13 03:46:48 +0100

    orig_prob evaluation complete

--- re_implementation/evaluate_YS.py
+++ re_implementation/evaluate_YS.py
@@ -1,10 +1,9 @@
-
+import collections
 import numpy as np
 import tensorflow as tf
 import tensorflow_datasets as tfds
 from re_implementation import dataset_utils_EC
 from re_implementation.helpers import model_helper
-
 
 """The evaluate(
     args:
@@ -17,12 +16,44 @@ from re_implementation.helpers import model_helper
     -return_dict=False, return the list
 """
 
-def evaluating(cvae, x_val):
+def evaluating(cvae, dataset, decoder_dist, dataset_type):
 
     # Evaluate on Validation data
+    x_train, x_val, x_test = dataset_utils_EC.get_dataset(dataset, decoder_dist, dataset_type)
     cvae.evaluate(x_val)
 
     # Computes an importance weighted log likelihood estimate.
+    origl_probs = collections.defaultdict(list)
+    corrct_probs = collections.defaultdict(list)
+
+    # Computes original log likelihood estimate.
+    in_sample = next(iter(x_val))
+    in_sample = np.expand_dims(in_sample, axis=0)
+    cvae.num_samples = 100
+    output = cvae.predict(in_sample)
+    klloss = cvae.kl_divergence_loss(in_sample, output['kl_divergence'])
+    if decoder_dist == "cBern":
+        recoloss = cvae.continuous_bernoulli_loss(in_sample, output['reconstruction'])
+    elif decoder_dist == "cat":
+        recoloss = cvae.categorical_loss(in_sample, output['reconstruction'])
+    #elbo = tf.reduce_logsumexp(recoloss - klloss, axis=0)
+    #elbo = elbo - tf.math.log(tf.cast(5, dtype=tf.float32))
+    elbo = recoloss - klloss
+    elbo = elbo - np.log(5)
+    origl_probs[dataset].append(elbo.numpy())
+
+    #Computes correction log likelihood estimate.
+    target = in_sample[1]
+    cvae.appy_correction = True
+    #corrct_probs[dataset].append(elbo -correct(target).sum(axis=(1, 2, 3)))
+
+    origl_probs[dataset] = np.concatenate(origl_probs[dataset], axis=0)
+    corrct_probs[dataset] = np.concatenate(corrct_probs[dataset], axis=0)
+
+    return {'orig_probs': origl_probs, 'corr_probs': corrct_probs}
+
+
+'''
     in_sample = next(iter(x_val))
     in_sample = np.expand_dims(in_sample, axis=0)
     cvae.num_samples = 100
@@ -38,8 +69,7 @@ def evaluating(cvae, x_val):
     vae_loss2 = loss1+loss2
     elbo = tf.reduce_logsumexp(vae_loss2 - vae_loss, axis=0)
     elbo = elbo - tf.math.log(tf.cast(100, dtype=tf.float32))
-
-    return elbo
+'''
 
 #thershold for loss etc?
 def thershold(model, x_latent, x_test):
@@ -81,6 +111,11 @@ cvae = model_helper.CVAE(
     decoder_dist=decoder_dist
 )
 
+cvae.compile(
+    optimizer=tf.keras.optimizers.Adam(learning_rate=5e-4),
+    loss={'reconstruction': cvae.get_reconstruction_loss_func(),
+          'kl_divergence': cvae.kl_divergence_loss}
+)
 cvae.load_weights(f'saved_models/{decoder_dist}/{dataset}/cvae-{method}/weights-{checkpoint_epoch}')
-x_train, x_val, _ = dataset_utils_EC.get_dataset(dataset, decoder_dist, dataset_type)
-pro_result = evaluating(cvae, x_val)
+# correction part
+pro_result = evaluating(cvae, dataset, decoder_dist, dataset_type)
